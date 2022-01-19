abd43a93e33ef432c353503eef4e4d059a53477d
Khasan <hasan.aliev.2000@mail.ru>
2022-01-19 17:33:38 +0600

    Initial commit.

---
+++ menu/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    slug = models.SlugField(primary_key=True)
+    title = models.CharField(max_length=55, unique=True)
+    image = models.ImageField(upload_to='category_images',
+                              null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    STATUS_CHOICES = (
+        ('in stock', 'в наличии'),
+    ("out of stock", 'нет в наличии')
+    )
+    name = models.CharField(max_length=155)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
+    image = models.ImageField(upload_to='images')
+    kkal = models.PositiveIntegerField()
+    category = models.ForeignKey(Category,
+                                 on_delete=models.CASCADE,
+                                 related_name='products')
+    status = models.CharField(max_length=15,
+                              choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return self.name
