107e97d519f31980f50ebc16c9ef80fb2ccc37c3
Khasan <hasan.aliev.2000@mail.ru>
2022-01-25 16:52:54 +0600

    Permission and Search.

--- menu/classviews.py
+++ menu/classviews.py
@@ -4,6 +4,33 @@ from django.views.generic import *
 
 from menu.forms import CreateProductForm, UpdateProductForm
 from menu.models import *
+from menu.permissions import IsAdminCheckMixin
+
+
+class SearchListView(ListView):
+    model = Product
+    template_name = 'search.html'
+    context_object_name = 'results'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(SearchListView, self).get_context_data()
+        context['search_word'] = self.request.GET.get('q')
+        return context
+
+
+    def get_queryset(self):
+        queryset = super(SearchListView, self).get_queryset()
+        search_word = self.request.GET.get('q')
+        if not search_word:
+            queryset = Product.objects.none()
+        else:
+            if len(search_word) < 3:
+                queryset = Product.objects.none()
+            else:
+                queryset = queryset.filter(name__icontains=search_word)
+            return queryset
+
+
 
 
 class CategoryListView(ListView):
@@ -23,7 +50,12 @@ class ProductListView(ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         slug = self.kwargs.get('slug')
-        queryset = queryset.filter(category__slug=slug)
+        filter_word = self.request.GET.get('filter')
+        if filter_word:
+            queryset = queryset.filter(category__slug=slug,
+                                       status=filter_word)
+        else:
+            queryset = queryset.filter(category__slug=slug)
         return queryset
 
 
@@ -42,7 +74,7 @@ class ProductDetailView(DetailView):
     pk_url_kwarg = 'product_id'
 
 
-class ProductCreateView(CreateView):
+class ProductCreateView(IsAdminCheckMixin, CreateView):
     model = Product
     template_name = 'create_product.html'
     form_class = CreateProductForm
@@ -57,7 +89,7 @@ class ProductCreateView(CreateView):
         return context
 
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(IsAdminCheckMixin, UpdateView):
     model = Product
     template_name = 'update_product.html'
     form_class = UpdateProductForm
@@ -70,7 +102,7 @@ class ProductUpdateView(UpdateView):
         return context
 
 
-class ProductDeleteView(DeleteView):
+class ProductDeleteView(IsAdminCheckMixin, DeleteView):
     model = Product
     template_name = 'delete_product.html'
     pk_url_kwarg = 'product_id'
