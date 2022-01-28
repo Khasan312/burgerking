ea0fc8dfaa7d3f9f4fd36534cb8528017e04dc46
Khasan312 <hasan.aliev.2000@mail.ru>
2022-01-28 11:13:59 +0600

    Order history.

--- order/urls.py
+++ order/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import order_create
+from .views import *
 
 urlpatterns = [
     path('create/', order_create, name='order-create'),
+    path('history/', order_history, name='order-history'),
+    path('history/detail/<int:order_id>/', order_history_detail, name='order-detail')
 
 ]
